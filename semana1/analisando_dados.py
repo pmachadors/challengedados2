05e1013d95f0cecee4ce329c82e1aec5ca7ab926
pmachadors <sotim182@gmail.com>
2022-11-23 19:20:45 -0300

    resolvido clausula where

--- semana1/analisando_dados.py
+++ semana1/analisando_dados.py
@@ -1,20 +1,23 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.master("local[1]") \
+spark = SparkSession.builder.master("local[*]") \
                     .appName('SparkAnalisandoDados') \
                     .getOrCreate()
 
 df = spark.read.json("dataset_bruto.json")
 
-print(df.count())
+# print(df.count())
 # 89083
 # df.show()
 
-#Bucando apenas as colunas de anuncio
+
 anuncio = df.select('anuncio.*')
 
-anuncio.filter(anuncio.tipo_uso == 'Residencial').show()
-# & anuncio.tipo_unidade == 'Apartamento' & anuncio.tipo_anuncio == 'Usado').show()
+filtro = anuncio\
+.select('*')\
+.where('tipo_uso == "Residencial"')\
+.where('tipo_unidade == "Apartamento"')\
+.where('tipo_anuncio == "Usado"').show()
 
 
 
