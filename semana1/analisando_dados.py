78234a8c0ca4179357881fbd054fe4c1ea529fcd
pmachadors <sotim182@gmail.com>
2022-11-25 15:03:37 -0300

    versao_final

--- semana1/analisando_dados.py
+++ semana1/analisando_dados.py
@@ -30,28 +30,29 @@ filtro_ = filtro_.withColumn("vaga",filtro.vaga[0].cast(IntegerType()))
 filtro_ = filtro_.withColumn("area_total",filtro.area_total[0].cast(IntegerType()))
 filtro_ = filtro_.withColumn("area_util",filtro.area_util[0].cast(IntegerType()))
 
-print(filtro_.count())
-
 # Extraindo bairro e zona e excluíndo endereco
 filtro_ = filtro_.withColumn('bairro',func.col('endereco.bairro'))
 filtro_ = filtro_.withColumn('zona',func.col('endereco.zona'))
 filtro_ = filtro_.drop(func.col("endereco"))
 
-print(filtro_.count())
-
 # Extraindo os valores do array valores
-valores = filtro_.withColumn('condominio',func.explode(func.col('valores.condominio')))
-valores = valores.withColumn('iptu',func.explode(func.col('valores.iptu')))
-valores = valores.withColumn('tipo',func.explode(func.col('valores.tipo')))
-valores = valores.withColumn('valor',func.explode(func.col('valores.valor')))
+valores = filtro_.withColumn('condominio',filtro_['valores']['condominio'][0].cast(DoubleType()))
+valores = valores.withColumn('iptu',valores['valores']['iptu'][0].cast(DoubleType()))
+valores = valores.withColumn('tipo',valores['valores']['tipo'][0])
+valores = valores.withColumn('valor',valores['valores']['valor'][0].cast(DoubleType()))
+# valores = filtro_.withColumn('condominio',func.explode(func.col('valores.condominio')))
+# valores = valores.withColumn('iptu',func.explode(func.col('valores.iptu')))
+# valores = valores.withColumn('tipo',func.explode(func.col('valores.tipo')))
+# valores = valores.withColumn('valor',func.explode(func.col('valores.valor')))
 valores = valores.drop('valores')
 
 # Filtrando apenas o tipo Venda
 valores_vendas = valores.select('*').where(func.col('tipo') == 'Venda')
-valores_vendas.show()
-
-# tentando entender por que os dataframes ficam com mais linhas com o explode
 
 # Salvando no formato parque
-valores_vendas.write.format('parquet').save('resultado_parquet')
+valores_vendas.write.format('parquet').save('valores_vendas_parquet')
+
+# Limpando caracteristicas pra salvar em csv
+valores_vendas_csv = valores_vendas.withColumn("caracteristicas",func.concat_ws(",",func.col("caracteristicas")))
+valores_vendas_csv.write.format('csv').save('valores_vendas_csv')
 
