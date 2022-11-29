5ac20546f8b34060b004ae45f5fcdaf6677545b9
pmachadors <sotim182@gmail.com>
2022-11-29 09:58:51 -0300

    semana 2

--- semana2/atividade.py
+++ semana2/atividade.py
@@ -3,7 +3,8 @@ from pyspark.sql import functions as f
 
 spark = SparkSession.builder.appName('semana2').getOrCreate()
 
-df_parquet = spark.read.parquet('dataset_transformado_parquet/part-00000-00341ba7-0a7c-4fef-a81e-1066725a64b1-c000.snappy.parquet')
+df_parquet = spark.read.parquet('semana2/dataset_transformado_parquet/part-00000-00341ba7-0a7c-4fef-a81e-1066725a64b1-c000.snappy.parquet')
 
 # Tratamento da coluna caracteristicas
-df_elementos_nulos = df_parquet.select(f.col('*'),f.when(f.element_at(df_parquet.caracteristicas, 1) == '',''))
+df_parquet.withColumn('caracteristicas',f.when(f.size(f.col('caracteristicas')) == 0, f.lit(None)).otherwise(f.col('caracteristicas'))).show()
+
